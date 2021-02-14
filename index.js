8443c77177227db84ee4d74e532d9a4244724e6f
James Canlas <jamescanlasjr@yahoo.com>
2021-02-13 16:16:03 -0800

    can now prompt questions

--- index.js
+++ index.js
@@ -1,13 +1,85 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your Github Username: (Required)', 
+    },
+    {
+        type: 'input',
+        name: 'repoName',
+        message: 'Enter your Github Repo Name: (Required)'
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the name of of your project? (Required)'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project: (Required)'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Describe the steps to install your project : '
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instuctions/examples of your project in use: '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What License did this project use? (Check all that apply)',
+        choices: [
+            'Apache',
+            'Boost',
+            'GNU GPL v3',
+            'GNU AGPL v3', 
+            'GNU LGPL v3', 
+            'GNU FDL v1.3', 
+            'ISC', 
+            'MIT', 
+            'Mozilla', 
+            'N/A']
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide guidelines on how other developers can contribute to your project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide any tests written for your application.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your Email: '
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt(     
+        questions
+    )
+}
 
 // Function call to initialize app
-init();
+init()
+    // .then(init => {
+    //     return generateMarkdown(init)
+    // });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// const renderLicenseBadge = (license) => {
+//   if(license === 'Apache 2.0'){
+//     return 'https://img.shields.io/badge/License-Apache%202.0-blue.svg'
+//   } else if(license === 'Boost') {
+//     return 'https://img.shields.io/badge/License-Boost%201.0-lightblue.svg'
+//   } else if(license === 'GNU GPL v3') {
+//     return 'https://img.shields.io/badge/License-GPLv3-blue.svg'
+//   } else if(license === 'GNU AGPL v3') {
+//     return ''
+//   } else if(license === 'GNU LGPL v3') {
+//     return ''
+//   } else if(license === 'GNU FDL v1.3') {
+//     return ''
+//   } else if(license === 'ISC') {
+//     return ''
+//   } else if(license === 'MIT') {
+//     return ''
+//   } else if(license === 'Mozilla') {
+//     return ''
+//   } else { // no license 
+//     return ''
+//   }
+// }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
